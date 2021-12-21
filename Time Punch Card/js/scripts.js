361a5b70a446cbad318dd9a081377c39bacf2f69
ifritborn <51976389+ifritborn@users.noreply.github.com>
2021-12-20 18:36:28 -0700

    Add files via upload

---
+++ Time Punch Card/js/scripts.js
@@ -0,0 +1,25 @@
+
+function logIn() {
+    var logIn = Date();
+    document.getElementById("logIn").innerHTML = logIn;
+  }
+  
+  function lunchStart() {
+    var lunchStart = Date();
+    document.getElementById("lunchStart").innerHTML = lunchStart;
+  }
+  
+  function lunchEnd() {
+    var lunchEnd = Date();
+    document.getElementById("lunchEnd").innerHTML = lunchEnd;
+  }
+  
+  function logOut() {
+    var logOut = Date();
+    document.getElementById("logOut").innerHTML = logOut;
+  }
+  
+  
+  
+  
+  
